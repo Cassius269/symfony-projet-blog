daf830cc5700fd6710d97b5acdc44b916bbb19ce
cassius <fahamygaston@gmail.com>
2024-06-12 15:43:27 +0200

    mise en place du header responsive

---
+++ assets/headerResponsive.js
@@ -0,0 +1,14 @@
+// Selection des élements
+let menuBurger = document.querySelector('.menuBurger');
+let nav = document.querySelector("nav");
+let iconeExitMenu = document.querySelector('.iconeExitMenu')
+// Gestionnaires d'évenements de type clic sur l'icone du menu burger et le menu exit
+menuBurger.addEventListener('click',()=>{
+    nav.classList.toggle('mobile-menu');
+    console.log("classe mobile")
+})
+
+
+iconeExitMenu.addEventListener('click',()=>{
+    nav.classList.toggle('mobile-menu');
+})
