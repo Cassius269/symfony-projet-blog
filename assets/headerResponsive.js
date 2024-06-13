5251821bd15f9d2e928abdfe44482f61e88bb938
cassius269 <fahamygaston@gmail.com>
2024-06-13 23:44:11 +0200

    le header est responsive et fonctionnel

--- assets/headerResponsive.js
+++ assets/headerResponsive.js
@@ -1,14 +1,17 @@
 // Selection des élements
 let menuBurger = document.querySelector('.menuBurger');
 let nav = document.querySelector("nav");
-let iconeExitMenu = document.querySelector('.iconeExitMenu')
+let iconeExitMenu = document.querySelector('.iconeExitMenu');
+let headerMobile = document.querySelector(".headerMobile");
+
 // Gestionnaires d'évenements de type clic sur l'icone du menu burger et le menu exit
 menuBurger.addEventListener('click',()=>{
     nav.classList.toggle('mobile-menu');
-    console.log("classe mobile")
-})
+});
 
 
 iconeExitMenu.addEventListener('click',()=>{
     nav.classList.toggle('mobile-menu');
-})
+});
+
+// Les médias queries
