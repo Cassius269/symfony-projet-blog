6bbe39b9980c77904f809735ff4c02071241e827
cassius269 <fahamygaston@gmail.com>
2024-06-14 22:43:24 +0200

    mise en place du controller Security et de la partie vue de la connexion utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,27 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     // Action pour enregistrer un utilisateur en base de données
-    #[Route('/register', name: 'register')]
-    public function register(): Response
+    #[Route(
+        path: '/register',
+        name: 'register'
+    )]
+    public function register(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
+        $user = new User();
+        $user->setFirstname('Fahami')
+            ->setLastname('MOHAMED ALI Fahami')
+            ->setEmail('fahamygaston@hotmail.com')
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($userPasswordHasher->hashPassword($user, '123456789')) // Par défaut utiliser un algo de sécurité comme bin2hex(random_bytes(10))
+            ->setAccepted(true)
+            ->setCreatedAt(new \DateTimeImmutable());
+
+
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         return $this->render('security/index.html.twig', []);
     }
 
 
     // Action pour connecter un utilisateur
+    #[Route(
+        path: '/login',
+        name: 'login'
+    )]
     public function connect(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $username = $authenticationUtils->getLastUsername();
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         if ($error) {
             $this->addFlash('error', 'Une erreur s\'est produite');
