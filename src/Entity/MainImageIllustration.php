b6f90401644749a2e014da98483002a611b11e1f
cassius269 <fahamygaston@gmail.com>
2024-09-03 18:04:31 +0200

    modernisation de la sérialisation de l'entité  MainImageIllustration

--- src/Entity/MainImageIllustration.php
+++ src/Entity/MainImageIllustration.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use Serializable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
@@ -14,7 +13,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: MainImageIllustrationRepository::class)]
 #[Vich\Uploadable]
-class MainImageIllustration implements Serializable
+class MainImageIllustration
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -28,7 +27,7 @@ class MainImageIllustration implements Serializable
     private ?string $imageName = null;
 
     #[Vich\UploadableField(mapping: 'articleIllustration', fileNameProperty: 'imageName')]
-    #[Ignore]
+    #[Ignore] // Ignorer la propriété $imageFile lors de la sérialisation car gérée par AWS S3 et Vich Uploader
     private ?File $imageFile = null;
 
     #[ORM\Column(length: 255, nullable: true)]
@@ -54,165 +53,29 @@ class MainImageIllustration implements Serializable
         $this->articles = new ArrayCollection();
     }
 
+    // ... other methods ...
 
-    public function getId(): ?int
+    public function __serialize(): array
     {
-        return $this->id;
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'imageName' => $this->imageName,
+            'source' => $this->source,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt,
+            // $this->imageFile n'est pas serialisée avec l'attribut ignore
+        ];
     }
 
-    public function getTitle(): ?string
+    public function __unserialize(array $data): void
     {
-        return $this->title;
-    }
-
-    public function setTitle(?string $title): static
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getSource(): ?string
-    {
-        return $this->source;
-    }
-
-    public function setSource(?string $source): static
-    {
-        $this->source = $source;
-
-        return $this;
-    }
-
-    public function getPhotographer(): ?Photographer
-    {
-        return $this->photographer;
-    }
-
-    public function setPhotographer(?Photographer $photographer): static
-    {
-        $this->photographer = $photographer;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Article>
-     */
-    public function getArticles(): Collection
-    {
-        return $this->articles;
-    }
-
-    public function addArticle(Article $article): static
-    {
-        if (!$this->articles->contains($article)) {
-            $this->articles->add($article);
-            $article->setMainImageIllustration($this);
-        }
-
-        return $this;
-    }
-
-    public function removeArticle(Article $article): static
-    {
-        if ($this->articles->removeElement($article)) {
-            // set the owning side to null (unless already changed)
-            if ($article->getMainImageIllustration() === $this) {
-                $article->setMainImageIllustration(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of imageFile
-     */
-    public function getImageFile()
-    {
-        return $this->imageFile;
-    }
-
-    /**
-     * Set the value of imageFile
-     *
-     * @return  self
-     */
-    public function setImageFile($imageFile)
-    {
-        $this->imageFile = $imageFile;
-        if (null !== $imageFile) {
-
-            // It is required that at least one field changes if you are using doctrine 
-
-            // otherwise the event listeners won't be called and the file is lost 
-
-            $this->updatedAt = new \DateTimeImmutable();
-        }
-    }
-
-    public function getImageName(): ?string
-    {
-        return $this->imageName;
-    }
-
-    public function setImageName(?string $imageName): static
-    {
-        $this->imageName = $imageName;
-
-        return $this;
-    }
-
-    // Fonctions de sérialisation et désérialisation d'un objet image d'illustration au moment de la mise à jour d'un article
-
-    public function serialize() // La méthode serialize permet de transformer les données en quelque chose de compréhensible et stockable en session
-    {
-        return serialize([
-            $this->id,
-            $this->title,
-            $this->imageName,
-            $this->source,
-            $this->createdAt,
-            $this->updatedAt,
-            // Ne pas sérialiser $this->imageFile car le fichier est envoyé à AWS S3 directement
-        ]);
-    }
-
-    public function unserialize($serialized) // permet de récuperer les données sérialisées
-    {
-        list(
-            $this->id,
-            $this->title,
-            $this->imageName,
-            $this->source,
-            $this->createdAt,
-            $this->updatedAt
-        ) = unserialize($serialized);
-        // $this->imageFile reste null après la désérialisation
+        $this->id = $data['id'];
+        $this->title = $data['title'];
+        $this->imageName = $data['imageName'];
+        $this->source = $data['source'];
+        $this->createdAt = $data['createdAt'];
+        $this->updatedAt = $data['updatedAt'];
+        // $this->imageFile reste null après la déserialization
     }
 }
